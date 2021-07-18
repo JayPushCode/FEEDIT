ffc7d0ced5b34cbfaa691c5fc0735c6fde040a3f
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-07-18 14:23:00 -0400

    moved second api, and added some slight const's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const cookEl = document.getElementById("cook");
 const APP_ID = "af4ea1f8";
 const APP_key = "70516f6e9f1db69f66850da24b13cac0";
-
+const searchQuery = "";
 
 const takeoutEl = document.getElementById("takeout");
 
@@ -12,46 +12,23 @@ cookEl.addEventListener("submit", (e) => {
 });
 
 async function fetchAPI() {
-    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
+    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=af4ea1f8&app_key=70516f6e9f1db69f66850da24b13cac0&from=0&to=20`;
     const response = await fetch(baseURL);
     const data = await response.json();
     generateHTML(data.hits);
-    console.log(foodsearch);
 }
 
 // https://api.edamam.com/search?q=pizza&app_id=af4ea1f8&app_key=70516f6e9f1db69f66850da24b13cac0&from=0&to=20
 
-const settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://worldwide-restaurants.p.rapidapi.com/typeahead",
-    "method": "POST",
-    "headers": {
-        "content-type": "application/x-www-form-urlencoded",
-        "x-rapidapi-key": "c93426bca1msh85e091a6e2679b7p1b7275jsn85ff73249622",
-        "x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com"
-    },
-    "data": {
-        "q": "band",
-        "language": "en_US"
-    }
-};
-
-$.ajax(settings).done(function(response) {
-    console.log(response);
-});
-
 // Code to Generate Results on Recipe.html
 
 const resultsEl = document.getElementById("recipe-results-container");
-
-
+const results = document.getElementById("foodsearch");
 
 function generateHTML(results) {
 
     let generatedHTML = "";
     results.map((result) => {
-        console.log(result);
         generatedHTML += `
         <div class="item">
           <img src="${result.recipe.image}" alt="img">
@@ -76,3 +53,25 @@ function generateHTML(results) {
 };
 
 $("#cook").on("click", generateHTML());
+
+// Resturant API
+
+// const settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": "https://worldwide-restaurants.p.rapidapi.com/typeahead",
+//   "method": "POST",
+//   "headers": {
+//       "content-type": "application/x-www-form-urlencoded",
+//       "x-rapidapi-key": "c93426bca1msh85e091a6e2679b7p1b7275jsn85ff73249622",
+//       "x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com"
+//   },
+//   "data": {
+//       "q": "band",
+//       "language": "en_US"
+//   }
+// };
+
+// $.ajax(settings).done(function(response) {
+//   console.log(response);
+// });
