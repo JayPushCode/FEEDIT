31a2f6bbebd426ad4a6695b09b1e83005fbd2120
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-07-25 11:56:14 -0400

    fixed our ingredients showing null, couldnt fix measurement of null just yet though

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,43 +9,6 @@ var drinkSearch = $("#drinksSearch");
 var drink = "";
 
 
-// past search
-var historyEl = document.getElementById("history");  
-var clearEL = document.getElementById("clear");
-let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-
-function renderSearchHistory() {
-    historyEl.innerHTML = "";
-    for (let i = 0; i < searchHistory.length; i++) {
-        const searchItem = document.createElement("input");
-        // <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"></input>
-        searchItem.setAttribute("type", "button");
-        searchItem.setAttribute("onclick", "reSearch(this)");
-        searchItem.setAttribute("style", "margin-bottom: 10px;")
-        searchItem.setAttribute("class", "form-control d-block bg-grey");
-        searchItem.setAttribute("id", "historyItem");
-        searchItem.setAttribute("value", searchHistory[i]);
-
-        historyEl.append(searchItem);
-    }
-}
-
-function reSearch(ele) {
-    let lastSearch = ele.value;
-    console.log("this is a test");
-    console.log(lastSearch);
-    food = lastSearch;
-    drink = lastSearch;
-
-    fetchData();
-    fetchDrinksData();
-
-}
-
-// This clears the search history when refreshing or going to a different page
-// not perfect but functional
-clearSearch();
-
 function clearSearch() {
     searchHistory = [];
     renderSearchHistory();
@@ -69,7 +32,7 @@ function fetchData() {
                         <h1 class="title">${data.recipe.label}</h1>
                         <a class="view-btn" target="_blank" href="${data.recipe.url}">View Recipe</a>
                     </div>
-
+                    <br>
                     <div class="data-details">
                     <p class="data">Calories: ${data.recipe.calories.toFixed(2)}</p>
                     <p class="data">Diet label: ${data.recipe.dietLabels.length > 0 ? data.recipe.dietLabels : "No Data Found"}</p>
@@ -94,10 +57,10 @@ $("#cook").click(function(event) {
     event.preventDefault();
     food = foodSearch.val();
     fetchData();
-    
+
     // Search history
     searchHistory.push(food);
-    localStorage.setItem("search",JSON.stringify(searchHistory));
+    localStorage.setItem("search", JSON.stringify(searchHistory));
     renderSearchHistory();
 });
 
@@ -115,22 +78,28 @@ function fetchDrinksData() {
             let drinks = data.drinks
             console.log(drinks)
             let html = drinks.map((data) => {
+                    let ingredients = '';
+                    let ingredientNum = 1;
+
+                    while (data[`strIngredient${ingredientNum}`]) {
+                        const ingredient = data[`strIngredient${ingredientNum}`];
+                        const measure = data[`strMeasure${ingredientNum}`];
+                        ingredients += `<p class="data">Ingredient ${ingredientNum}: ${measure}: ${ingredient}</p>`;
+                        ingredientNum++;
+                    }
+
                     return ` <div class="item">
-                    <div id="imgBx"> 
-                      <img id="resultImg" src=${data.strDrinkThumb} alt="Drink Image"</img>
-                      <h1 class="title">${data.strDrink}</h1>
-                    </div>
-                    <div class="data-details">
-                      <p class="data">Ingredient 1: ${data.strMeasure1}: ${data.strIngredient1}</p>
-                      <p class="data">Ingredient 2: ${data.strMeasure2}: ${data.strIngredient2}</p>
-                      <p class="data">Ingredient 3: ${data.strMeasure3}: ${data.strIngredient3}</p>
-                      <p class="data">Ingredient 4: ${data.strMeasure4}: ${data.strIngredient4}</p>
-                      <p class="data">Ingredient 5: ${data.strMeasure5}: ${data.strIngredient5}</p>
-                    </div>
-                    <div class="instructions">
-                      <p class="data">Instructions: ${data.strInstructions}</p>
-                    </div>
-                </div>`;
+                <div id="imgBx"> 
+                  <img id="resultImg" src=${data.strDrinkThumb} alt="Drink Image"</img>
+                  <h1 class="title">${data.strDrink}</h1>
+                </div>
+                <div class="data-details">
+                    ${ingredients}
+                </div>
+                <div class="instructions">
+                  <p class="data">Instructions: ${data.strInstructions}</p>
+                </div>
+            </div>`;
                 })
                 .join("");
             document
@@ -151,7 +120,7 @@ $("#drink").click(function(event) {
 
     // Search history
     searchHistory.push(drink);
-    localStorage.setItem("search",JSON.stringify(searchHistory));
+    localStorage.setItem("search", JSON.stringify(searchHistory));
     renderSearchHistory();
 });
 
